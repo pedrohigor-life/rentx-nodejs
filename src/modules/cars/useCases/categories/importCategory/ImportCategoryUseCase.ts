b03859d3c47a4bd28cc915083995dcce45fb7405
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-04-04 19:58:09 -0300

    Refatorando importação de categorias por CSV

--- src/modules/cars/useCases/categories/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/categories/importCategory/ImportCategoryUseCase.ts
@@ -1,24 +1,56 @@
 import fs from 'fs';
+
 import { parse } from 'csv-parse';
 import { ICategoriesRepositories } from '../../../repositories/ICategoriesRepositories';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
   constructor(private categoriesRepositories: ICategoriesRepositories) {}
 
-  execute(file: Express.Multer.File) {
-    const stream = fs.createReadStream(file.path);
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportCategory[] = [];
 
-    const parser = parse({
-      delimiter: ',',
-    });
+      const stream = fs.createReadStream(file.path);
 
-    stream.pipe(parser);
-
-    parser.on('data', async (line) => {
-      this.categoriesRepositories.create({
-        name: line[0],
-        description: line[1],
+      const parser = parse({
+        delimiter: ',',
       });
+
+      stream.pipe(parser);
+
+      parser
+        .on('data', async (line) => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const categoryAlreadyExists =
+        this.categoriesRepositories.findByCategory(name);
+
+      if (!categoryAlreadyExists)
+        this.categoriesRepositories.create({ name, description });
     });
   }
 }
