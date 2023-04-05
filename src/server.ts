9444ba7117d3a16bc4196a6b9b8f0762dac2da5f
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-04-04 22:12:57 -0300

    Iniciando a documentação: Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
+
+import { categoriesRoutes } from './modules/routes/categoriesRoutes';
+import { specificationsRoutes } from './modules/routes/specificationRoutes';
+
+/**
+ * Express
+ */
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Swagger
+ */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 /**
  * Routes
  */
-import { categoriesRoutes } from './modules/routes/categoriesRoutes';
-import { specificationsRoutes } from './modules/routes/specificationRoutes';
-
-const app = express();
-
-app.use(express.json());
 app.use('/categories', categoriesRoutes);
 app.use('/specifications', specificationsRoutes);
 
 app.listen(3333, () => {
-  console.log('[APP] Server is runnin ...');
+  console.log('[APP] Server is running ...');
 });
