02b064324c6544e50584c3eb24d5bfa77f10ee0f
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-04-19 18:57:39 -0300

    Removendo módulo de contas de usuários e configurando typeorm para acessar o banco sem utilizar a rede dos containers

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,7 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
 
-/**
- * Routes
- */
-import { categoriesRoutes } from './modules/routes/categoriesRoutes/categories.routes';
-import { specificationsRoutes } from './modules/routes/specificationRoutes/specificaitons.routes';
-import { userRoutes } from './modules/routes/usersRoutes/usersRoutes.routes';
+import { routes } from './modules/routes';
 
 /**
  * Database
@@ -24,20 +19,17 @@ import './shared/container';
  * Express
  */
 const app = express();
-
 app.use(express.json());
+
+/**
+ * Routes
+ */
+app.use(routes);
 
 /**
  * Swagger
  */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-/**
- * Routes
- */
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
-app.use('/users', userRoutes);
 
 app.listen(3333, () => {
   console.log('[APP] Server is running ...');
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,10 +1,9 @@
 import { container } from 'tsyringe';
+
 import { ICategoriesRepositories } from '../../modules/cars/repositories/ICategoriesRepositories';
 import { CategoriesRepositories } from '../../modules/cars/repositories/implementations/CategoriesRepositories';
 import { ISpecificationsRepositories } from '../../modules/cars/repositories/ISpecificationsRepositories';
 import { SpecificationsRepositories } from '../../modules/cars/repositories/implementations/SpecificationsRepositories';
-import { IUserRepositories } from '../../modules/accounts/repositories/IUsersRepositories';
-import { UsersRepositories } from '../../modules/accounts/repositories/implementations/UsersRepositories';
 
 container.registerSingleton<ICategoriesRepositories>(
   'CategoriesRepository',
@@ -14,9 +13,4 @@ container.registerSingleton<ICategoriesRepositories>(
 container.registerSingleton<ISpecificationsRepositories>(
   'SpecificationsRepository',
   SpecificationsRepositories
-);
-
-container.registerSingleton<IUserRepositories>(
-  'UserRepository',
-  UsersRepositories
 );
