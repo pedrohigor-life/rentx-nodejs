a6da80ef6d509ce580f236e9557994d482f62918
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-05-22 17:43:46 -0300

    Implementando testes

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,5 @@
+import { AppError } from '../../../../errors/AppErrors';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { UserRepositoryInMemory } from '../../repositories/in-memory/UserRepositoryInMemory';
 import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
 import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
@@ -13,7 +15,7 @@ describe('Authenticate user', () => {
     authenticateUserUseCase = new AuthenticateUserUseCase(userRepository);
   });
   it('should be able authenticate user', async () => {
-    const account = {
+    const account: ICreateUserDTO = {
       name: 'test user',
       email: 'test@gmail.com',
       password: '12345678',
@@ -33,5 +35,37 @@ describe('Authenticate user', () => {
     });
 
     expect(userAuthenticated).toHaveProperty('token');
+  });
+
+  it('should be not able to authenticate an noexistent user', () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: 'false@gmail.com',
+        password: '12345678',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with incorret password', () => {
+    expect(async () => {
+      const account: ICreateUserDTO = {
+        name: 'Incorrect password',
+        email: 'incorrect.assword@gmail.com',
+        password: '1234567',
+        driver_license: '3287432874632',
+      };
+
+      await createUserUseCase.execute({
+        name: account.name,
+        email: account.email,
+        driver_license: account.driver_license,
+        password: account.password,
+      });
+
+      await authenticateUserUseCase.execute({
+        email: account.email,
+        password: 'incorrectPassword',
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
